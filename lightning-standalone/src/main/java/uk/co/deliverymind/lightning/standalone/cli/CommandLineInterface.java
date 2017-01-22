b789a8dbf650007ada949bb6c0e35d2ff1d2efb0
Irek Pastusiak <irek@deliverymind.co.uk>
2017-01-22 13:03:13 +0000

    Add version information to help output

--- lightning-standalone/src/main/java/uk/co/deliverymind/lightning/standalone/cli/CommandLineInterface.java
+++ lightning-standalone/src/main/java/uk/co/deliverymind/lightning/standalone/cli/CommandLineInterface.java
@@ -3,11 +3,18 @@ package uk.co.deliverymind.lightning.standalone.cli;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.Parameters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import uk.co.deliverymind.lightning.standalone.cli.commands.CommandReport;
 import uk.co.deliverymind.lightning.standalone.cli.commands.CommandVerify;
 
+import java.io.IOException;
+import java.util.Properties;
+
 @Parameters(separators = "=")
 public class CommandLineInterface {
+
+    private static final Logger logger = LoggerFactory.getLogger(CommandLineInterface.class);
 
     @Parameter(names = {"-h", "--help"}, help = true, hidden = true)
     private boolean help;
@@ -33,12 +40,28 @@ public class CommandLineInterface {
     }
 
     public void printHelp() {
-        jc.setProgramName("java -jar lightning-<version_number>.jar");
+        jc.setProgramName(String.format("java -jar lightning-%s.jar", getVersion()));
         jc.usage();
     }
 
     public String getParsedCommand() {
         return jc.getParsedCommand();
+    }
+
+    private String getVersion() {
+        Properties prop = new Properties();
+        try {
+            prop.load(getClass().getClassLoader().getResourceAsStream("version.properties"));
+        } catch (IOException | NullPointerException e) {
+            logger.error("Error reading version.properties");
+            System.exit(1);
+        }
+        String version = prop.getProperty("version");
+        if (null == version) {
+            logger.error("Error reading version from version.properties");
+            System.exit(1);
+        }
+        return version;
     }
 
 }
