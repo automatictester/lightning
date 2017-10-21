3fd9543847b68e37857373eb41da7e51422e73e8
Irek Pastusiak <irek@deliverymind.co.uk>
2017-10-21 11:12:19 +0100

    Move to multibranch pipeline

--- jobdsl.groovy
+++ jobdsl.groovy
@@ -1,81 +1,42 @@
 #!groovy
 
-def gitCreds = 'github-creds'
-def folderName = 'lightning'
+folder('lightning')
 
+['core', 'standalone', 'maven', 'gradle'].each { module ->
 
-// Create folder with Pipeline Plugin Folder-Level Shared Library
-folder(folderName) {
-    properties {
-        folderLibraries {
-            libraries {
-                libraryConfiguration {
-                    name 'SharedLib'
-                    retriever {
-                        modernSCM {
-                            scm {
-                                git {
-                                    id 'lightning-git-repo'
-                                    remote 'git@github.com:deliverymind/lightning.git'
-                                    credentialsId gitCreds
+    multibranchPipelineJob("lightning/${module}") {
+        branchSources {
+            git {
+                remote('git@github.com:deliverymind/lightning.git')
+                credentialsId('github-creds')
+            }
+        }
+        properties {
+            folderLibraries {
+                libraries {
+                    libraryConfiguration {
+                        name 'SharedLib'
+                        retriever {
+                            modernSCM {
+                                scm {
+                                    git {
+                                        id 'lightning-git-repo'
+                                        remote 'git@github.com:deliverymind/lightning.git'
+                                        credentialsId 'github-creds'
+                                    }
                                 }
                             }
                         }
-                    }
-                    allowVersionOverride true
-                    defaultVersion 'master'
-                    implicit true
-                }
-            }
-        }
-    }
-}
-
-
-// Create separate jobs for every Lightning module
-['core', 'standalone', 'maven', 'gradle'].each { module ->
-    pipelineJob("$folderName/${module}") {
-        concurrentBuild(false)
-        properties {
-            buildDiscarder {
-                strategy {
-                    logRotator {
-                        numToKeepStr('10')
-                        daysToKeepStr(null)
-                        artifactDaysToKeepStr(null)
-                        artifactNumToKeepStr(null)
+                        allowVersionOverride true
+                        defaultVersion 'master'
+                        implicit true
                     }
                 }
             }
-            githubProjectProperty {
-                projectUrlStr('https://github.com/deliverymind/lightning/')
-            }
-            rebuildSettings {
-                autoRebuild(true)
-                rebuildDisabled(false)
-            }
         }
-        parameters {
-            stringParam('RELEASE_VERSION', '9.0.0', '9.1.0, 9.2.0 etc')
-            stringParam('POST_RELEASE_SNAPSHOT_VERSION', '9.0.1-SNAPSHOT', '9.1.1-SNAPSHOT, 9.2.1-SNAPSHOT etc')
-            nonStoredPasswordParam('GPG_PASSPHRASE', 'GPG passphrase for signing artefacts')
-            stringParam('REPO_URL', 'git@github.com:deliverymind/lightning.git', 'Or local path, e.g.: "file:///Users/username/git/lightning"')
-            stringParam('GIT_CREDENTIALS_ID', gitCreds, '')
-            stringParam('GIT_BRANCH', 'master', '')
-            stringParam('TEST_ONLY', 'true', '')
-            stringParam('DRY_RUN', 'true', '')
-        }
-        definition {
-            cpsScm {
-                scm {
-                    git {
-                        branch('*/master')
-                        remote {
-                            credentials(gitCreds)
-                            url('$REPO_URL')
-                        }
-                    }
-                }
+        configure {
+            it / factory(class: 'org.jenkinsci.plugins.workflow.multibranch.WorkflowBranchProjectFactory') {
+                owner(class: 'org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject', reference: '../..')
                 scriptPath("jenkins/${module}/Jenkinsfile")
             }
         }
--- src/SharedLib.groovy
+++ src/SharedLib.groovy
@@ -1,21 +1,15 @@
-// Jenkins Pipeline Plugin Folder-Level Shared Library
 
 def cleanupWorkspaceAndCloneRepo() {
     step([$class: 'WsCleanup'])
-    git credentialsId: "${GIT_CREDENTIALS_ID}", url: "${REPO_URL}", branch: "${GIT_BRANCH}"
+    git credentialsId: 'github-creds', url: 'git@github.com:deliverymind/lightning.git'
 }
 
 def purge() {
     sh 'rm -rf ~/.m2/repository/uk/co/deliverymind/'
-
-//    does not work correctly in all cases
-//    withEnv(["PATH+MAVEN=${tool 'M3'}/bin"]) {
-//        sh 'mvn -pl plugin dependency:purge-local-repository -DresolutionFuzziness=groupId -Dinclude=uk.co.deliverymind -DactTransitively=false -DreResolve=false -Dverbose=true'
-//    }
 }
 
 def push() {
-    sshagent(["${GIT_CREDENTIALS_ID}"]) {
-        sh "git push --set-upstream origin master; git push --tags"
+    sshagent(['github-creds']) {
+        sh 'git push --set-upstream origin master; git push --tags'
     }
 }
