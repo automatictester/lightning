f8702241b862b7923e92c087064af343c2579b3f
Irek Pastusiak <irek@deliverymind.co.uk>
2018-02-11 16:49:37 +0000

    Remove lombok to shrink fat jar size

--- lightning-aws-lambda/src/main/java/uk/co/deliverymind/lightning/lambda/LightningRequest.java
+++ lightning-aws-lambda/src/main/java/uk/co/deliverymind/lightning/lambda/LightningRequest.java
@@ -1,19 +1,62 @@
 package uk.co.deliverymind.lightning.lambda;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class LightningRequest {
 
     private String bucket;
     private String region;
-
     private String mode;
     private String xml;
     private String jmeterCsv;
     private String perfmonCsv;
+
+    public LightningRequest() {
+    }
+
+    public String getBucket() {
+        return bucket;
+    }
+
+    public void setBucket(String bucket) {
+        this.bucket = bucket;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+
+    public String getXml() {
+        return xml;
+    }
+
+    public void setXml(String xml) {
+        this.xml = xml;
+    }
+
+    public String getJmeterCsv() {
+        return jmeterCsv;
+    }
+
+    public void setJmeterCsv(String jmeterCsv) {
+        this.jmeterCsv = jmeterCsv;
+    }
+
+    public String getPerfmonCsv() {
+        return perfmonCsv;
+    }
+
+    public void setPerfmonCsv(String perfmonCsv) {
+        this.perfmonCsv = perfmonCsv;
+    }
 }
