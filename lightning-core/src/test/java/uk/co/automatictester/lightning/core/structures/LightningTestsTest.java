57f2ebd612d6487174bcb1d443834e31491d119c
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2018-08-30 19:53:13 +0100

    Fix random test failures

--- lightning-core/src/test/java/uk/co/automatictester/lightning/core/structures/LightningTestsTest.java
+++ lightning-core/src/test/java/uk/co/automatictester/lightning/core/structures/LightningTestsTest.java
@@ -11,6 +11,7 @@ public class LightningTestsTest {
     @Test
     public void testGetInstance() {
         LightningTests testsA = LightningTests.getInstance();
+        testsA.flush();
         RespTimeAvgTest test = new RespTimeAvgTest.Builder("my name", 800).build();
         testsA.add(test);
 
@@ -22,6 +23,7 @@ public class LightningTestsTest {
     @Test
     public void testFlush() {
         LightningTests tests = LightningTests.getInstance();
+        tests.flush();
         RespTimeAvgTest test = new RespTimeAvgTest.Builder("my name", 800).build();
         tests.add(test);
         assertThat(tests.size() == 1, is(true));
