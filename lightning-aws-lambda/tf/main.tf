ae37c99307cf00d898fe5704a3dd17202d18dace
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2018-11-19 09:58:26 +0000

    Apply formatting

--- lightning-aws-lambda/tf/main.tf
+++ lightning-aws-lambda/tf/main.tf
@@ -1,24 +1,23 @@
 provider "aws" {
-    region = "eu-west-2"
+  region = "eu-west-2"
 }
 
 terraform {
-    backend "s3" {
+  backend "s3" {
     bucket = "automatictester.co.uk-lightning-aws-lambda-tf-state"
-    key    = "lightning-lambda.tfstate"
+    key = "lightning-lambda.tfstate"
     region = "eu-west-2"
-    }
+  }
 }
 
 resource "aws_lambda_function" "lightning_ci" {
-
-    function_name = "Lightning"
-    handler = "uk.co.automatictester.lightning.lambda.LightningHandler"
-    runtime = "java8"
-    s3_bucket = "automatictester.co.uk-lightning-aws-lambda-jar"
-    s3_key = "lightning-aws-lambda.jar"
-    source_code_hash = "${base64sha256(file("${path.module}/../target/lightning-aws-lambda.jar"))}"
-    role = "arn:aws:iam::611654469811:role/LightningLambda"
-    memory_size = "${var.memory}"
-    timeout = "${var.timeout}"
+  function_name = "Lightning"
+  handler = "uk.co.automatictester.lightning.lambda.LightningHandler"
+  runtime = "java8"
+  s3_bucket = "automatictester.co.uk-lightning-aws-lambda-jar"
+  s3_key = "lightning-aws-lambda.jar"
+  source_code_hash = "${base64sha256(file("${path.module}/../target/lightning-aws-lambda.jar"))}"
+  role = "arn:aws:iam::611654469811:role/LightningLambda"
+  memory_size = "${var.memory}"
+  timeout = "${var.timeout}"
 }
