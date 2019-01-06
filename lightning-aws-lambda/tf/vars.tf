fe576d402a820996fc77974dfbbad752d17bfcd8
Ireneusz Pastusiak <ireneusz.pastusiak@gmail.com>
2019-01-06 15:16:01 +0000

    Extend terraform

--- lightning-aws-lambda/tf/vars.tf
+++ lightning-aws-lambda/tf/vars.tf
@@ -1,7 +1,12 @@
-variable "memory" {
-    default = "256"
+variable "s3_bucket_jar" {
+  default = "automatictester.co.uk-lightning-aws-lambda-jar"
 }
-
+variable "s3_bucket_data" {
+  default = "automatictester.co.uk-lightning-aws-lambda"
+}
+variable "memory" {
+  default = "256"
+}
 variable "timeout" {
-    default = "30"
+  default = "30"
 }
